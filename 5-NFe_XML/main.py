69c6a6d60a07d8a7df1319af5982524edfcb8e32
Roney <41809912+rschaskos@users.noreply.github.com>
2024-10-10 15:16:47 -0300

    Update main.py
    split numbers by dot

--- 5-NFe_XML/main.py
+++ 5-NFe_XML/main.py
@@ -7,7 +7,7 @@ head = ['Empresa', 'N da NF', 'Data Emissao',
         'Valor Unitario do Produto', 'Valor do Produto',
         'Valor de Desconto']
 
-LOCAL = r'C:\Users\seu-usuario\local\arquivo'
+LOCAL = r'C:\Users\seu_usuario\Downloads\XML'
 CSV_PATH = os.path.join(LOCAL, 'excel.csv')
 
 with open(CSV_PATH, 'w', newline='', encoding='utf-8') as output:
@@ -34,13 +34,16 @@ for filename in os.listdir(LOCAL):
                     cProd = prod.getElementsByTagName('cProd')[0].firstChild.data
                     xProd = prod.getElementsByTagName('xProd')[0].firstChild.data
                     qProd = prod.getElementsByTagName('qCom')[0].firstChild.data
+                    qProd_split = qProd.split('.')[0]
                     vUnProd = prod.getElementsByTagName('vUnCom')[0].firstChild.data
+                    vUnProd_split = vUnProd.split('.')[0]
                     vProd = prod.getElementsByTagName('vProd')[0].firstChild.data
+                    vProd_split = vProd.split('.')[0]
                     vDesc_elements = prod.getElementsByTagName('vDesc')
                     vDesc = vDesc_elements[0].firstChild.data if vDesc_elements else '0.00'
 
                     # Escrever os dados no CSV
-                    writer.writerow([cnpj_emit, num_nfe, data_nfe, cProd, xProd, qProd, vUnProd, vProd, vDesc])
+                    writer.writerow([cnpj_emit, num_nfe, data_nfe, cProd, xProd, qProd_split, vUnProd_split, vProd_split, vDesc])
                     print(cnpj_emit, num_nfe, data_nfe, cProd, xProd, qProd, vUnProd, vProd)
 print(f'\nSALVO NO LOCAL: {CSV_PATH}')
 print('SEU RELATÓRIO FOI CONCLUÍDO COM SUCESSO')
