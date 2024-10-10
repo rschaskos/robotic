1dc89d73a83ba3fcb99dd9daf9c3cdd171daa5eb
Roney <41809912+rschaskos@users.noreply.github.com>
2024-10-09 22:07:29 -0300

    Update main.py

--- 4-Antecedentes/main.py
+++ 4-Antecedentes/main.py
@@ -1,3 +1,7 @@
+#########################################
+#   DESENVOLVIDO POR RSCHASKOS EM 2024  #
+#########################################
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
