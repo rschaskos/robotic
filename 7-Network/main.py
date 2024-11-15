43b22ddec465d852521d2eb89eca3bac8be422c9
Roney <41809912+rschaskos@users.noreply.github.com>
2024-11-15 09:12:46 -0300

    New implementation

--- 7-Network/main.py
+++ 7-Network/main.py
@@ -34,26 +34,40 @@ def is_host_active(ip):
     return response == 0
 
 # Rede a ser verificada
-rede = ipaddress.ip_network('192.168.5.0/24')
+redes = [ipaddress.ip_network('192.168.4.0/24'), ipaddress.ip_network('192.168.5.0/24')]
+# Rede a ser ignorada
+ip_ignore = [ipaddress.ip_address(ip) for ip in [
+    "192.168.4.1", "192.168.4.9", "192.168.4.13", "192.168.4.21", "192.168.4.22", "192.168.4.23", "192.168.4.25", "192.168.4.26",
+    "192.168.4.27", "192.168.4.28", "192.168.4.29", "192.168.4.30", "192.168.4.32", "192.168.4.33", "192.168.4.34", "192.168.4.37", "192.168.4.38",
+    "192.168.4.39", "192.168.4.42", "192.168.4.43", "192.168.4.44", "192.168.4.46", "192.168.4.47", "192.168.4.48", "192.168.4.50", "192.168.4.52",
+    "192.168.4.55", "192.168.4.56", "192.168.4.61", "192.168.4.62", "192.168.4.63", "192.168.4.67", "192.168.4.69", "192.168.4.72", "192.168.4.75",
+    "192.168.4.81", "192.168.4.91", "192.168.4.101", "192.168.4.123", "192.168.4.125", "192.168.4.140", "192.168.4.202", "192.168.4.203", "192.168.4.204",
+    "192.168.4.208", "192.168.4.209", "192.168.4.213", "192.168.4.214", "192.168.4.215", "192.168.4.218", "192.168.4.221", "192.168.4.222", "192.168.4.224", 
+    "192.168.4.226", "192.168.4.228", "192.168.4.231", "192.168.4.232","192.168.4.234", "192.168.4.235", "192.168.4.251", "192.168.4.254", "192.168.5.68", 
+    "192.168.5.74", "192.168.5.131", "192.168.5.191", "192.168.5.208", "192.168.5.209", "192.168.5.212", "192.168.5.215", "192.168.5.250", "192.168.5.254"
+]]
 
 # Abrir os arquivos em modo de escrita
 with open(arquivo_ip_mail, 'w') as f_ip, open(arquivo_status, 'w') as f_status, open(arquivo_powershell, 'w') as f_ps:
-    active = 0
-    deactive = 0
-    for ip in rede:
-        if is_host_active(ip):
-            active += 1
-            f_ip.write(f'{ip}\n')
-            f_ps.write(f'{ip}\n')
-        else:
-            deactive += 1
-        status = f'IP: {ip}'
-        print(status)
-        f_status.write(f'{status}\n')
-    resumo_all = f'\nAll List IPs...\n\nActive ip: {active}\nDeactive ip: {deactive}\nTime: {hour_adjust}'
-    resumo_ip_active = f'\nOnly Active IPs...\n\nActive ip: {active}\nDeactive ip: {deactive}\nTime: {hour_adjust}'
-    print(resumo_all)
-    f_ip.write(resumo_ip_active)
+    for rede in redes:
+        active = 0
+        deactive = 0
+        for ip in rede:
+            if ip in ip_ignore:
+                    continue
+            if is_host_active(ip):
+                active += 1
+                f_ip.write(f'{ip}\n')
+                f_ps.write(f'{ip}\n')
+            else:
+                deactive += 1
+            status = f'IP: {ip}'
+            print(status)
+            f_status.write(f'{status}\n')
+        resumo_all = f'\nAll List IPs...\n\nActive ip: {active}\nDeactive ip: {deactive}\nTime: {hour_adjust}'
+        resumo_ip_active = f'\nOnly Active IPs...\n\nActive ip: {active}\nDeactive ip: {deactive}\nTime: {hour_adjust}\n'
+        print(resumo_all)
+        f_ip.write(resumo_ip_active)
 
 # Função para enviar e-mail
 def enviar_email(assunto, mensagem, destinatario, anexo):
@@ -88,7 +102,7 @@ def enviar_email(assunto, mensagem, destinatario, anexo):
 
 # Enviar o e-mail com o relatório
 enviar_email(
-    assunto="Relatório de Status da Rede",
+    assunto=f"Relatório de Status da Rede - {hour_adjust}",
     mensagem="O relatório de status da rede com os IPs ativos está em anexo.",
     destinatario=EMAIL_DESTINATARIO,
     anexo=arquivo_ip_mail
